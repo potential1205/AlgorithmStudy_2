841e712479dead920dfb901ca4f77ca84a537ea3
potential1205 <potential1205@naver.com>
2023-02-14 17:37:42 +0900

    week5

---
+++ jaehoon/week5/17071_P5_숨바꼭질5.py
@@ -0,0 +1,39 @@
+
+def solve(N,K,points,time):
+
+    if N != K:
+        while K < 500001:
+            next_points = set()
+            for subin in points:
+                for nxt in (subin-1, subin+1, subin*2):
+                    if -1 < nxt < 500001:
+                        flag = time % 2
+                        if move[nxt][flag] < 0:
+                            move[nxt][flag] = time
+                            next_points.add(nxt)
+
+            points = next_points
+
+            K += time
+            if K < 500001:
+                flag = time % 2
+                if move[K][flag] > -1:
+                    break           
+
+                time += 1
+    else:
+        time = 0
+
+    if K < 500001:
+        print(time)
+    else:
+        print(-1)
+
+
+if __name__ == "__main__":
+    N, K = map(int, input().split())
+    time = 1
+    move = [[-1, -1] for _ in range(500001)]
+    move[N][0] = 0
+    points = set([N])
+    solve(N,K,points,time)
